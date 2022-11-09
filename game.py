a947b43902719d08cd0fa6707904063a4c80f215
Daniel <rippedchili@gmail.com>
2022-11-09 19:03:36 -0400

    Birtday Game Updated

--- game.py
+++ game.py
@@ -2,81 +2,19 @@ from random import randint
 
 name = input("Hi! What is your name?")
 
-guess_number = 1
-month_number = randint(1, 12)
-year_number = randint(1924, 2004)
+for guess_number in range(1, 6):
+    month_number = randint(1, 12)
+    year_number = randint(1924, 2004)
 
-print("Guess 1:", name, "were you born in",
-     month_number, "/", year_number, "?")
+    print("Guess", guess_number, ": were you born in",
+        month_number, "/", year_number, "?")
 
-response = input("yes or no?")
+    response = input("yes or no?")
 
-if response == "yes":
-    print("I knew it!")
-    exit()
-else:
-    print("Drat! Lemme try again!")
-
-# Guess 2
-
-month_number = randint(1, 12)
-year_number = randint(1924, 2004)
-
-print("Guess 2:", name, "were you born in",
-    month_number, "/", year_number, "?")
-
-response = input("yes or no? ")
-
-if response == "yes":
-    print("I knew it!")
-    exit()
-else:
-    print("Drat! Lemme try again!")
-
-# Guess 3
-
-month_number = randint(1, 12)
-year_number = randint(1924, 2004)
-
-print("Guess 3:", name, "were you born in",
-    month_number, "/", year_number, "?")
-
-response = input("yes or no? ")
-
-if response == "yes":
-    print("I knew it!")
-    exit()
-else:
-    print("Drat! Lemme try again!")
-
-# Guess 4
-
-month_number = randint(1, 12)
-year_number = randint(1924, 2004)
-
-print("Guess 4:", name, "were you born in",
-    month_number, "/", year_number, "?")
-
-response = input("yes or no? ")
-
-if response == "yes":
-    print("I knew it!")
-    exit()
-else:
-    print("Drat! Lemme try again!")
-
-# Guess 5
-
-month_number = randint(1, 12)
-year_number = randint(1924, 2004)
-
-print("Guess 5:", name, "were you born in",
-    month_number, "/", year_number, "?")
-
-response = input("yes or no? ")
-
-if response == "yes":
-    print("I knew it!")
-    exit()
-else:
-    print("I dont wanna do this anymore!")
+    if response == "yes":
+        print("I knew it!")
+        exit()
+    elif guess_number == 5:
+        print("I don't want to do this anymore")
+    else:
+        print("Drat! Lemme try again!")
