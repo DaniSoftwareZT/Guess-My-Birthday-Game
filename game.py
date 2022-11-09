13003a50afcb578e9e5d28852a8a9b641a45aacb
Daniel <rippedchili@gmail.com>
2022-11-09 14:42:21 -0400

    Wrote a game

---
+++ game.py
@@ -0,0 +1,14 @@
+from random import randint
+guess_number = 1
+month_number = generate a random number
+year_number = generate a random number
+
+print this message:
+    "Guess " + guess_number + " : " + name + "were you" +
+    "born on " + month_number + " / " + year_number + " ? "
+response = prompt the player with "yes or no?"
+
+if response is "yes", then
+    print the message "I knew it!"
+otherwise,
+    print the message "Drat! Lemme try again!"
