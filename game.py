2240bed8261f86629b688a7498c814dc5ccbc5d9
Daniel <rippedchili@gmail.com>
2022-11-09 15:08:06 -0400

    Wrote a new game

--- game.py
+++ game.py
@@ -1,14 +1,82 @@
 from random import randint
+
+name = input("Hi! What is your name?")
+
 guess_number = 1
-month_number = generate a random number
-year_number = generate a random number
+month_number = randint(1, 12)
+year_number = randint(1924, 2004)
 
-print this message:
-    "Guess " + guess_number + " : " + name + "were you" +
-    "born on " + month_number + " / " + year_number + " ? "
-response = prompt the player with "yes or no?"
+print("Guess 1:", name, "were you born in",
+     month_number, "/", year_number, "?")
 
-if response is "yes", then
-    print the message "I knew it!"
-otherwise,
-    print the message "Drat! Lemme try again!"
+response = input("yes or no?")
+
+if response == "yes":
+    print("I knew it!")
+    exit()
+else:
+    print("Drat! Lemme try again!")
+
+# Guess 2
+
+month_number = randint(1, 12)
+year_number = randint(1924, 2004)
+
+print("Guess 2:", name, "were you born in",
+    month_number, "/", year_number, "?")
+
+response = input("yes or no? ")
+
+if response == "yes":
+    print("I knew it!")
+    exit()
+else:
+    print("Drat! Lemme try again!")
+
+# Guess 3
+
+month_number = randint(1, 12)
+year_number = randint(1924, 2004)
+
+print("Guess 3:", name, "were you born in",
+    month_number, "/", year_number, "?")
+
+response = input("yes or no? ")
+
+if response == "yes":
+    print("I knew it!")
+    exit()
+else:
+    print("Drat! Lemme try again!")
+
+# Guess 4
+
+month_number = randint(1, 12)
+year_number = randint(1924, 2004)
+
+print("Guess 4:", name, "were you born in",
+    month_number, "/", year_number, "?")
+
+response = input("yes or no? ")
+
+if response == "yes":
+    print("I knew it!")
+    exit()
+else:
+    print("Drat! Lemme try again!")
+
+# Guess 5
+
+month_number = randint(1, 12)
+year_number = randint(1924, 2004)
+
+print("Guess 5:", name, "were you born in",
+    month_number, "/", year_number, "?")
+
+response = input("yes or no? ")
+
+if response == "yes":
+    print("I knew it!")
+    exit()
+else:
+    print("I dont wanna do this anymore!")
